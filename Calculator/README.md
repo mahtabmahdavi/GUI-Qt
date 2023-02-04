e2a5f794730f27ad9d423f834d29b0e08d99640c
Mahtab Mahdavi <mahtaabmahdavi@gmail.com>
2023-02-05 02:21:02 +0330

    add some extra features

--- Calculator/README.md
+++ Calculator/README.md
@@ -1,14 +1,6 @@
 # Calculator
 
-This calculator is designed using [pyside6](https://doc.qt.io/qtforpython/) library. By using this calculator, you can have the following features:
-- sin
-- cos
-- tan
-- cot
-- log
-- mod
-- power
-- square root
+This calculator is designed using [pyside6](https://doc.qt.io/qtforpython/) library. In this version of the calculator, there are features such as power, square root, some trigonometric functions, logarithm base 10 and mod.
 
 ---
 
@@ -24,6 +16,4 @@ Then you can run this program:
 python main.py
 ```
 
-**NOTE :** For using sin, cos, tan, cot, log and square root buttons, first enter the number, then press the operator button and equal button.
-
-![Calculator](calculator.jpg)
+![Calculator](Mahtab'sCalculator.jpg)
--- Calculator/main.py
+++ Calculator/main.py
@@ -9,6 +9,7 @@ from functools import partial
 class Calculator(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.num = ""
 
         loader = QUiLoader()
         self.ui = loader.load("calculator.ui")
@@ -24,82 +25,92 @@ class Calculator(QMainWindow):
         self.ui.btn_7.clicked.connect(partial(self.show_number, "7"))
         self.ui.btn_8.clicked.connect(partial(self.show_number, "8"))
         self.ui.btn_9.clicked.connect(partial(self.show_number, "9"))
+
+        self.ui.btn_pi.clicked.connect(partial(self.show_number, f"{pi}"))
+        self.ui.btn_open.clicked.connect(partial(self.show_number, "("))
+        self.ui.btn_closed.clicked.connect(partial(self.show_number, ")"))
         self.ui.btn_dot.clicked.connect(partial(self.show_number, "."))
-        self.ui.btn_negative.clicked.connect(partial(self.show_symbol, "-"))
-        self.ui.btn_percent.clicked.connect(partial(self.show_symbol, "%"))
-
-        self.ui.btn_sum.clicked.connect(partial(self.calculate, "sum"))
-        self.ui.btn_subtract.clicked.connect(partial(self.calculate, "subtract"))
-        self.ui.btn_multiply.clicked.connect(partial(self.calculate, "multiply"))
-        self.ui.btn_divide.clicked.connect(partial(self.calculate, "divide"))
-        self.ui.btn_power.clicked.connect(partial(self.calculate, "power"))
-        self.ui.btn_square_root.clicked.connect(partial(self.calculate, "square root"))
-
-        self.ui.btn_sin.clicked.connect(partial(self.show_symbol, "sin"))
-        self.ui.btn_cos.clicked.connect(partial(self.show_symbol, "cos"))
-        self.ui.btn_tan.clicked.connect(partial(self.show_symbol, "tan"))
-        self.ui.btn_cot.clicked.connect(partial(self.show_symbol, "cot"))
-        self.ui.btn_log.clicked.connect(partial(self.show_symbol, "log"))
-        self.ui.btn_mod.clicked.connect(partial(self.show_symbol, "mod"))
+        self.ui.btn_negative.clicked.connect(partial(self.show_symbol, "-", "neg"))
+        self.ui.btn_percent.clicked.connect(partial(self.show_symbol, "%", "pct"))
+        
+        self.ui.btn_sum.clicked.connect(partial(self.show_symbol, "+", "sum"))
+        self.ui.btn_subtract.clicked.connect(partial(self.show_symbol, "-", "sub"))
+        self.ui.btn_multiply.clicked.connect(partial(self.show_symbol, "×", "mul"))
+        self.ui.btn_divide.clicked.connect(partial(self.show_symbol, "÷", "div"))
+        self.ui.btn_power.clicked.connect(partial(self.show_symbol, "^", "pow"))
+        self.ui.btn_square_root.clicked.connect(partial(self.show_symbol, "√", "sqrt"))
+        self.ui.btn_sin.clicked.connect(partial(self.show_symbol, "sin", "sin"))
+        self.ui.btn_cos.clicked.connect(partial(self.show_symbol, "cos", "cos"))
+        self.ui.btn_tan.clicked.connect(partial(self.show_symbol, "tan", "tan"))
+        self.ui.btn_cot.clicked.connect(partial(self.show_symbol, "cot", "cot"))
+        self.ui.btn_log.clicked.connect(partial(self.show_symbol, "log", "log"))
+        self.ui.btn_mod.clicked.connect(partial(self.show_symbol, "mod", "mod"))
         
         self.ui.btn_ac.clicked.connect(self.reset)
         self.ui.btn_equal.clicked.connect(self.equal)
 
+    # Methods
     def show_number(self, number):
+        self.num += number
         self.ui.input_text.setText(self.ui.input_text.text() + number)
+        
+    def show_symbol(self, symbol, mode):
+        self.mode = mode
+        if self.mode == "sin" or self.mode == "cos" or self.mode == "tan" or self.mode == "cot" or self.mode == "log":
+            self.ui.input_text.setText(self.ui.input_text.text() + symbol + " ")
+        else:
+            self.ui.input_text.setText(self.ui.input_text.text() + symbol)
 
-    def show_symbol(self, symbol):
-        self.mode = symbol
-        self.num1 = float(self.ui.input_text.text())
-        self.ui.input_text.setText(symbol)
-
-    def calculate(self, symbol):
-        self.mode = symbol
-        self.num1 = float(self.ui.input_text.text())
-        self.ui.input_text.setText("")
+    def calculate(self):
+        if self.mode == "pct":
+            self.expression = self.expression.split("%")
+            return float(self.expression[0]) * 0.01
+        elif self.mode == "sqrt":
+            if self.num >= 0:
+                return sqrt(float(self.num))
+            else:
+                return "ERROR"
+        elif self.mode == "sin":
+            return sin(radians(float(self.num)))
+        elif self.mode == "cos":
+            return cos(radians(float(self.num)))
+        elif self.mode == "tan":
+            return tan(radians(float(self.num)))
+        elif self.mode == "cot":
+            return 1 / tan(radians(float(self.num)))
+        elif self.mode == "log":
+            return log10(float(self.num))
+        else:
+            try:
+                return float(eval(self.expression))
+            except:
+                return "ERROR"
 
     def equal(self):
-        if self.mode == "sum":
-            self.num2 = float(self.ui.input_text.text())
-            result = self.num1 + self.num2
-        elif self.mode == "subtract":
-            self.num2 = float(self.ui.input_text.text())
-            result = self.num1 - self.num2
-        elif self.mode == "multiply":
-            self.num2 = float(self.ui.input_text.text())
-            result = self.num1 * self.num2
-        elif self.mode == "divide":
-            self.num2 = float(self.ui.input_text.text())
-            if self.num2 != 0:
-                result = self.num1 / self.num2
-            else:
-                result = "ERROR"
-        elif self.mode == "power":
-            self.num2 = float(self.ui.input_text.text())
-            result = self.num1 ** self.num2
-        elif self.mode == "square root":
-            result = sqrt(self.num1)
-        elif self.mode == "sin":
-            result = sin(radians(self.num1))
-        elif self.mode == "cos":
-            result = cos(radians(self.num1))
-        elif self.mode == "tan":
-            result = tan(radians(self.num1))
-        elif self.mode == "cot":
-            result = 1 / tan(radians(self.num1))
-        elif self.mode == "log":
-            result = log10(self.num1)
-        elif self.mode == "mod":
-            self.num2 = float(self.ui.input_text
-.text())
-            result = self.num1 % self.num2
-        self.ui.input_text.setText(str(result))
+        self.output = self.ui.input_text.text()
+        self.expression = self.output
 
+        if "×" in self.expression:
+            self.expression = self.expression.replace("×", "*")
+        if "÷" in self.expression:
+            self.expression = self.expression.replace("÷", "/")
+        if "^" in self.expression:
+            self.expression = self.expression.replace("^", "**")
+        if "mod" in self.expression:
+            self.expression = self.expression.replace("mod", "%")
+            
+        result = str(self.calculate())
+        self.output += ("=" + result)
+        self.ui.input_text.setText(result)
+        self.ui.output_text.setText(f"  {self.ui.output_text.text()}\n  {self.output}")
+    
     def reset(self):
+        self.num = ""
         self.ui.input_text.setText("")
         
+# ----- End of Calculator class and its methods ----- #
 
 if __name__ == "__main__":
-    my_app = QApplication()
+    app = QApplication()
     main_window = Calculator()
-    my_app.exec()
+    app.exec()
