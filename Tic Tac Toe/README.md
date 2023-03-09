a7c354b9268a56fa85e8389a2ced715226ea03f8
Mahtab Mahdavi <mahtaabmahdavi@gmail.com>
2023-03-09 22:20:15 +0330

    main.py updated and README.md added to this repo

---
+++ Tic Tac Toe/README.md
@@ -0,0 +1,30 @@
+# ❌ Tic Tac Toe ⭕
+
+This game is designed using [Qt](https://doc.qt.io/qtforpython/) library in python.
+
+Tic Tac Toe is a simple game for two players. The first player who can quickly fill a row or a column or diagonally a 3 x 3 square wins the game. If all the cells of the table are filled and no one succeeds in completing the mentioned conditions, the game will be tied.
+
+---
+## Game Environment 
+
+Click on the cell with your mouse to play. You can only place your pieces in the cells that are empty.
+
+This game has two modes :
+- ### Play with CPU
+![Play with CPU](Images/play_with_cpu.jpg)
+
+- ### Play with player
+![Play with Player](Images/play_with_player.jpg)
+
+---
+## Usage 🎮
+
+First install requirement package:
+```
+pip install PySide6
+```
+Then you can run this program:
+```
+python main.py
+```
+**NOTE :** Please put the file with extension ui in the same path as main.py.
--- Tic Tac Toe/main.py
+++ Tic Tac Toe/main.py
@@ -7,72 +7,112 @@ from PySide6.QtUiTools import QUiLoader
 
 class Tic_Tac_Toe(QMainWindow):
     def __init__(self):
-        # Properties
+        super().__init__()
         loader = QUiLoader()
         self.main_window = loader.load("tic_tac_toe.ui")
         self.main_window.show()
-
-        self.mode = None
-        self.player = 1
+        # Properties
         self.sign = "X"
-        self.draw = 0
-        self.your_score = 0
-        self.opponent_score = 0
+        self.tie = 0
+        self.cpu_score = 0
+        self.player_1_score = 0
+        self.player_2_score = 0
         self.buttons = [[self.main_window.btn_1, self.main_window.btn_2, self.main_window.btn_3],
                         [self.main_window.btn_4, self.main_window.btn_5, self.main_window.btn_6],
                         [self.main_window.btn_7, self.main_window.btn_8, self.main_window.btn_9]]
-
+        self.main_window.rb_cpu.setChecked(True)
+        self.game_type()
         for i in range(3):
             for j in range(3):
                 self.buttons[i][j].clicked.connect(partial(self.play, i, j))
-
-        #self.main_window.btn_new_game.clicked.connect()
-        #self.main_window.btn_about.clicked.connect()
-        self.main_window.rb_cpu.toggled.connect(self.type_of_game)
-        self.main_window.rb_player_2.toggled.connect(self.type_of_game)
+        self.main_window.btn_new_game.clicked.connect(self.reset_game)
+        self.main_window.btn_about.clicked.connect(self.show_about)
+        self.main_window.rb_cpu.toggled.connect(self.game_type)
+        self.main_window.rb_player_2.toggled.connect(self.game_type)
              
     # Methods
-    def type_of_game(self):
-        self.main_window.lbl_x.setText(f"X (YOU)\n{self.your_score}")
-        self.main_window.lbl_draw.setText(f"DRAW\n{self.draw}")
+    def game_type(self):
+        self.sign = "X"
+        self.tie = 0
+        self.cpu_score = 0
+        self.player_1_score = 0
+        self.player_2_score = 0
+        self.reset_game()
         if self.main_window.rb_cpu.isChecked():
-            self.main_window.lbl_o.setText(f"O (CPU)\n{self.opponent_score}")
             self.mode = "CPU"
-        else:
-            self.main_window.lbl_o.setText(f"O (PLAYER)\n{self.opponent_score}")
+        elif self.main_window.rb_player_2.isChecked():
             self.mode = "Player"
+        self.show_turn()
+        self.show_score()
+    
+    def show_turn(self):
+        if self.sign == "X":
+            self.main_window.lbl_turn.setText(f"{self.sign} TURN")
+        else:
+            self.main_window.lbl_turn.setText(f"{self.sign} TURN")
+    
+    def show_score(self):
+        self.main_window.lbl_x.setText(f"X (YOU)\n{self.player_1_score}")
+        self.main_window.lbl_tie.setText(f"TIE\n{self.tie}")
+        if self.mode == "CPU":
+            self.main_window.lbl_o.setText(f"O (CPU)\n{self.cpu_score}")
+        elif self.mode == "Player":
+            self.main_window.lbl_o.setText(f"O (PLAYER)\n{self.player_2_score}")
 
     def play(self, row, col):
         if self.buttons[row][col].text() == "":
-            if self.player == 1:
+            if self.sign == "X":
                 self.buttons[row][col].setText(self.sign)
                 self.buttons[row][col].setStyleSheet("color: #2CC6BC; background-color: #1F3540; border-radius: 5px;")
-                self.check()
-                self.player = 2
+                self.check_game()
                 self.sign = "O"
+                self.show_turn()
                 if self.mode == "CPU":
-                    self.play(random.randint(0, 2), random.randint(0, 2))
+                    temp_row = random.randint(0, 2)
+                    temp_col = random.randint(0, 2)
+                    while self.buttons[temp_row][temp_col].text() != "":
+                        temp_row = random.randint(0, 2)
+                        temp_col = random.randint(0, 2)
+                    self.play(temp_row, temp_col) 
             else:
                 self.buttons[row][col].setText(self.sign)
                 self.buttons[row][col].setStyleSheet("color: #F0B430; background-color: #1F3540; border-radius: 5px;")
-                self.check()
-                self.player = 1
+                self.check_game()
                 self.sign = "X"
+                self.show_turn()
 
-    def check(self):
+    def check_game(self):
         if self.check_win() == True:
+            message_box = QMessageBox()
+            message_box.setWindowTitle("Congratulations")
             if self.sign == "X":
-                message_box = QMessageBox(text="Congratulations to you 🎉")
-                message_box.exec()
+                self.player_1_score += 1
+                self.show_score()
+                if self.mode == "CPU":
+                    message_box.setText("You win the game 🥂        ")
+                else:
+                    message_box.setText("Player 1 wins the game 🎉      ")
             else:
-                message_box = QMessageBox(text="Congratulations to Player 2 🎉")
-                message_box.exec()
-        
+                if self.mode == "CPU":
+                    self.cpu_score += 1
+                    self.show_score()
+                    message_box.setText("Computer wins the game 🙄      ")
+                else:
+                    self.player_2_score += 1
+                    self.show_score()
+                    message_box.setText("Player 2 wins the game 🎉     ")
+            message_box.exec()
+            self.reset_game()
         else:
-            if self.check_draw() == True:
-                message_box = QMessageBox(text="Both of you are equal!")
+            if self.check_tie() == True:
+                self.tie += 1
+                self.show_score()
+                message_box = QMessageBox(text = "Both of you are equal 🤝      ")
+                message_box.setWindowTitle("Tie")
                 message_box.exec()
-               
+                if self.mode == "CPU":
+                    self.reset_game()
+     
     def check_win(self):
         win_checker = False
         for i in range(3):
@@ -83,14 +123,30 @@ class Tic_Tac_Toe(QMainWindow):
             win_checker = True
         return win_checker
 
-    def check_draw(self):
+    def check_tie(self):
         for i in range(len(self.buttons)):
             for j in range(len(self.buttons[i])):
                 if self.buttons[i][j].text() == "":
                     return False
         else:
             return True
-            
+
+    def reset_game(self):
+        for i in range(3):
+            for j in range(3):
+                self.buttons[i][j].setText("")
+
+    def show_about(self):
+        message_box = QMessageBox(text = """  Tic Tac Toe is a simple game for two players.
+The first player who can quickly fill a row or a column or diagonally
+a 3 x 3 square wins the game. If all the cells of the table are filled
+and no one succeeds in completing the mentioned conditions,
+the game will be tied.""")
+        message_box.setWindowTitle("About")
+        message_box.exec()
+
+# -------------------- End of Game Class -------------------- #
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ui = Tic_Tac_Toe()
